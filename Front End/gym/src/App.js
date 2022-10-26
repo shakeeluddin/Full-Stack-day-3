f546560c5c91b677a977b132943a09855b640acb
shakeeluddin <s_uddin91@hotmail.com>
2022-10-26 15:59:34 +0100

    Updated Version

---
+++ Front End/gym/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Form from './components/form';
+function App() {
+  return (
+
+
+    <Form/>
+
+
+
+
+
+    
+  
+  )}
+export default App;
---
+++ Front End/gym/src/components/form.jsx
@@ -0,0 +1,39 @@
+function Form() {
+    return (
+        <form>
+
+            <h2>Gym Routine</h2>
+
+            <label for="Day">Day</label>
+            <input type="text" name=""></input>
+            <br></br>
+            <br></br>
+            <label for="">Breakfast</label>
+            <input type="text" name=""></input>
+            <br></br>
+            <br></br>
+            <label for="">Stretches</label>
+            <input type="text" name=""></input>
+            <br></br>
+            <br></br>
+            <label for="">Exercise 1</label>
+            <input type="text" name=""></input>
+            <br></br>
+            <br></br>
+            <label for="">Exercise 2</label>
+            <input type="text" name=""></input>
+            <br></br>
+            <br></br>
+            <label for="">Exercise 3</label>
+            <input type="text" name=""></input>
+            <br></br>
+            <br></br>
+
+            <input type = "submit" value="submit"></input>
+
+        </form>
+    )
+
+}
+
+export default Form;
